f61145e0a04ff66addbe59b6352c6ed300345792
+ <+>
2022-06-09 16:36:05 -0400

    commit tree b959e96a2551e1554f8e95f539f5fbaa9584e43b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log('Hello');
+}sayHello()
+function sayHelloGuadalupe() {
+    console.log('Hello Guadalupe!')
+}
+sayHelloGuadalupe()
+function sayHelloLiz() {
+    console.log('Hello Liz!')
+}
+sayHelloLiz()
+
